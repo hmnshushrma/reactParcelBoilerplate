7dbff9de588c7ba527072d05aa86ad7fa06a9260
hmnshushrma <himanshu.sharma1@freecharge.com>
2023-10-28 03:03:16 +0530

    initial parcel setup

---
+++ src/app/app.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Button from "@mui/material/Button";
+export function App() {
+  return (
+    <>
+      <h1>Hello world!</h1>
+      <Button variant="contained">Hello world</Button>
+    </>
+  );
+}
